d709775c6cab5a22d3aee36a85fd88fa0b518bbf
TranPhucVinh <tranphucvinh96@gmail.com>
2018-10-04 23:42:57 +0700

    edit pool variable

--- routes/app/validate.js
+++ routes/app/validate.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 var querystring = require('querystring');
 var url = require("url");
-const pool = require('./../database/database');
+const pool = require('./../../database/database');
 
 router.post("/", validation);
 function validation(req, res) {
