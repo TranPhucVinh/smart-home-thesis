822c2a22860ed38ebf4d3e209a120878933b3767
TranPhucVinh <tranphucvinh96@gmail.com>
2018-10-05 14:23:06 +0700

    add array to validate.js

--- routes/app/validate.js
+++ routes/app/validate.js
@@ -4,11 +4,14 @@ var querystring = require('querystring');
 var url = require("url");
 const pool = require('./../../database/database');
 
+var returnArray = [];
+
 router.post("/", validation);
 function validation(req, res) {
   var uriData = url.parse(req.url);	
   var queryData = querystring.parse(uriData.query);
-  res.send(queryData);
+  returnArray.push(queryData);
+  res.send(returnArray);
 }
 
-module.exports = router;
+module.exports = router; 
