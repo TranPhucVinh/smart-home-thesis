d3ac7890ca3d0c398a8ab2217c638726ff4dca61
TranPhucVinh <tranphucvinh96@gmail.com>
2018-11-06 10:55:54 +0700

    testing with API

--- routes/app/app.js
+++ routes/app/app.js
@@ -148,7 +148,7 @@ function device(req, res){
   var deviceJSON = {"id": "555", "status": "555"};
   deviceJSON.id = "19";
   // deviceJSON.status = req.body.status;
-  res.send(deviceJSON+" "+req.body.name);
+  res.send(deviceJSON+" "+req.body);
 }
 
 module.exports = router; 
