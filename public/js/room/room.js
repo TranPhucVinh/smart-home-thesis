88c919b45ae20784480904905b16575dc72624d7
TranPhucVinh <tranphucvinh96@gmail.com>
2018-11-08 04:30:27 +0700

    testing API for app

--- public/js/room/room.js
+++ public/js/room/room.js
@@ -66,6 +66,7 @@ $(document).ready(function(){
     }
     }, 1000);
 
+setTimeout(function() {
         $.ajax({url: "app/app.device", type:"POST",
             async: true,
             data: {sendArray: statusArray},
@@ -73,6 +74,7 @@ $(document).ready(function(){
             console.log(statusArray);
         }
     });
+}, 1500);
 
         $(".delete-device").click(function(){
         var id = $(this).attr("data-id");
